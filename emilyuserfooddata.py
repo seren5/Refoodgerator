e47896100f26f6d150e4eb8a07bd3e3ccc993918
Emily Ren <150500495+seren5@users.noreply.github.com>
2023-11-11 20:48:38 +0000

    push

---
+++ emilyuserfooddata.py
@@ -0,0 +1,15 @@
+def user_food():
+    food_item = input("Please input the food item you'd like to store in lowercase and non-plural (Check your spelling! Our varieties are limited to the general categories, such as cabbage)")
+    if is_vegetable(food_item):
+        
+    is_fruit(food_item)
+
+def is_vegetable(item):
+    vegetable_list = [artichoke, asparagus, eggplant, avocado, beet, bok choy, broccoli, brussels sprout, cabbage, carrot]
+    aubergine == eggplant
+
+def is_fruit(item):
+    fruit = []
+
+def other():
+    e = 2
